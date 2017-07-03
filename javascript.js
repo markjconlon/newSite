01e06feed2dc037c5439da84762fd4dbda63b047
markjconlon <markjconlon@gmail.com>
2017-07-02 22:41:19 -0400

    added links and pictures to hoptimal, cardShuffler as well as js to go bewteen

---
+++ javascript.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+  var current = 0;
+  var pastProjectsIndex = ["pp1", "pp2"];
+
+  $("#lArrow").on("click", function(){
+    $("#" + pastProjectsIndex[current]).addClass('displayNone');
+    if (current <= 0) {
+      let newIndex = (pastProjectsIndex.length) - 1;
+      $("#" + pastProjectsIndex[newIndex]).removeClass('displayNone');
+      current = newIndex;
+    } else {
+      $("#" + pastProjectsIndex[current - 1]).removeClass('displayNone');
+      current -= 1;
+    }
+  });
+  
+  $("#rArrow").on("click", function(){
+    $("#" + pastProjectsIndex[current]).addClass('displayNone');
+    if (current >= pastProjectsIndex.length - 1 ) {
+      $("#" + pastProjectsIndex[0]).removeClass('displayNone');
+      current = 0;
+    } else {
+      $("#" + pastProjectsIndex[current + 1]).removeClass('displayNone');
+      current += 1;
+    }
+  });
+
+});
